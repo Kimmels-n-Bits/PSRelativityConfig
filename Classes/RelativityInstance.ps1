c7380502b698ddab417e9fd293b4e4c8c151cdb5
Jarrod Kimmel <jk@oasisdiscovery.net>
2023-11-23 08:33:17 -0800

    refactored RelativityServer class

--- Classes/RelativityInstance.ps1
+++ Classes/RelativityInstance.ps1
@@ -1,3 +1,378 @@
+<#
+.SYNOPSIS
+  Enum for defining server roles within a Relativity environment.
+
+.DESCRIPTION
+  The RelativityServerRole enum enumerates the various roles that a server can be assigned in a Relativity deployment.
+  Each role corresponds to specific functionalities and responsibilities within the system.
+
+.EXAMPLE
+  [RelativityServerRole]::Agent
+  Demonstrates how to reference the Agent role from the enum.
+
+.NOTES
+#>
+enum RelativityServerRole
+{
+    Agent
+    Analytics
+    DistributedSql
+    PrimarySql
+    SecretStore
+    ServiceBus
+    Web
+    Worker
+    WorkerManager
+}
+
+<#
+.SYNOPSIS
+  Represents a server in a Relativity environment, encapsulating roles, status, and configuration.
+
+.DESCRIPTION
+  The RelativityServer class models a server within a Relativity environment. It holds information about the server's roles,
+  online status, and configuration properties necessary for each role. The class provides methods to manage server roles,
+  configure properties, and validate server readiness.
+
+.EXAMPLE
+  $server = [RelativityServer]::new("ServerName")
+  Creates a new RelativityServer instance with the specified server name.
+
+.EXAMPLE
+  $server.AddRole([RelativityServerRole]::Agent)
+  Adds the Agent role to the server and configures related properties.
+
+.PARAMETER Name
+  The name of the server. This should be a valid network name.
+
+.NOTES
+#>
+class RelativityServer
+{
+    [ValidateNotNullOrEmpty()]
+    [String] $Name
+    [ValidateNotNull()]
+    [Boolean] $IsOnline
+    [System.Collections.Generic.HashSet[RelativityServerRole]] $Role
+    [Hashtable] $ResponseFileProperties
+
+    hidden static [Hashtable] $RoleResponseFileProperties = @{
+        [RelativityServerRole]::Agent = @(
+            "InstallAgents",
+            "InstallDir",
+            "PrimarySqlInstance",
+            "UseWinAuth",
+            "DefaultAgents"
+        )
+        [RelativityServerRole]::Analytics = @(
+
+        )
+        [RelativityServerRole]::DistributedSql = @(
+            "InstallDistributedDatabase",
+            "InstallDir",
+            "PrimarySqlInstance",
+            "UseWinAuth",
+            "DistributedSqlInstance",
+            "DatabaseBackupDir",
+            "LdfDir",
+            "MdfDir",
+            "FullTextDir"
+        )
+        [RelativityServerRole]::PrimarySql = @(
+            "InstallPrimaryDatabase",
+            "InstallDir",
+            "PrimarySqlInstance",
+            "UseWinAuth",
+            "DefaultFileRepository",
+            "EddsFileShare",
+            "CacheLocation",
+            "DtSearchIndexPath",
+            "RelativityInstanceName",
+            "DatabaseBackupDir",
+            "LdfDir",
+            "MdfDir",
+            "FullTextDir"
+        )
+        [RelativityServerRole]::SecretStore = @(
+            "SqlInstanceServerName",
+            "UseWinAuth",
+            "InstallDir"
+        )
+        [RelativityServerRole]::ServiceBus = @(
+            "InstallServiceBus",
+            "InstallDir",
+            "PrimarySqlInstance",
+            "UseWinAuth",
+            "ServiceBusProvider",
+            "ServerFQDN",
+            "TlsEnabled"
+        )
+        [RelativityServerRole]::Web = @(
+            "InstallWeb",
+            "InstallDir",
+            "PrimarySqlInstance",
+            "UseWinAuth",
+            "EnableWinAuth"
+        )
+        [RelativityServerRole]::Worker = @(
+            "InstallWorker",
+            "SqlInstance",
+            "RelativitySqlInstance",
+            "WorkerInstallPath"
+        )
+        [RelativityServerRole]::WorkerManager = @(
+            "InstallQueueManager",
+            "SqlInstance",
+            "RelativitySqlInstance",
+            "UseWinAuth",
+            "WorkerNetworkPath",
+            "IdentityServerUrl",
+            "QueueManagerInstallPath",
+            "MdfDir",
+            "LdfDir",
+            "DtSearchIndexPath",
+            "DataFilesNetworkPath",
+            "NistPackagePath"
+        )
+    }
+
+    RelativityServer([String] $name)
+    {
+        try
+        {
+            Write-Verbose "Creating an instance of RelativityServer."
+            $this.Name = $name
+            $this.Role = New-Object 'System.Collections.Generic.HashSet[RelativityServerRole]'
+            $this.ResponseFileProperties = @{}
+            $this.TestConnection()
+            Write-Verbose "Created an instance of RelativityServer."
+        }
+        catch
+        {
+            Write-Error "An error occurred while creating an instance of RelativityServer for $($this.Name)."
+            throw
+        }
+    }
+
+    <#
+    .SYNOPSIS
+    Adds a role to the server and configures default properties for that role.
+
+    .DESCRIPTION
+    The AddRole method assigns a specified role to the server. It automatically populates the server's
+    configuration with default properties relevant to the role, avoiding overwriting existing values.
+
+    .PARAMETER role
+    The role to be added to the server. Must be a valid value from RelativityServerRole enum.
+
+    .EXAMPLE
+    $server.AddRole([RelativityServerRole]::Agent)
+    Adds the Agent role to the server.
+
+    .NOTES
+    #>
+    [void] AddRole([RelativityServerRole] $role)
+    {
+        try
+        {
+            if (-not $this.Role.Contains($role))
+            {
+                Write-Verbose "Adding $($role) role to $($this.Name)."
+                $this.Role.Add($role)
+                foreach ($Property in [RelativityServer]::RoleResponseFileProperties[$role])
+                {
+                    if (-not $this.ResponseFileProperties.ContainsKey($property))
+                    {
+                        Write-Verbose "Adding $($Property) Property to $($this.Name)."
+                        switch ($Property)
+                        {
+                            "InstallAgents" { $this.ResponseFileProperties[$Property] = "1" }
+                            "InstallDistributedDatabase" { $this.ResponseFileProperties[$Property] = "1" }
+                            "InstallPrimaryDatabase" { $this.ResponseFileProperties[$Property] = "1" }
+                            "InstallServiceBus" { $this.ResponseFileProperties[$Property] = "1" }
+                            "InstallWeb" { $this.ResponseFileProperties[$Property] = "1" }
+                            "InstallWorker" { $this.ResponseFileProperties[$Property] = "1" }
+                            "InstallQueueManager" { $this.ResponseFileProperties[$Property] = "1" }
+                            "InstallDir" { $this.ResponseFileProperties[$Property] = "C:\Program Files\kCura Corporation\Relativity\" }
+                            "UseWinAuth" { $this.ResponseFileProperties[$Property] = "1" }
+                            "DefaultAgents" { $this.ResponseFileProperties[$Property] = "0" }
+                            "WorkerInstallPath" { $this.ResponseFileProperties[$Property] = "C:\Program Files\kCura Corporation\Invariant\Worker\" }
+                            "QueueManagerInstallPath" { $this.ResponseFileProperties[$Property] = "C:\Program Files\kCura Corporation\Invariant\QueueManager\" }
+
+                            default { $this.ResponseFileProperties[$Property] = $null }
+                        }
+                        Write-Verbose "Added $($Property) Property to $($this.Name)."
+                    }
+                }
+                Write-Verbose "Added $($role) role to $($this.Name)."
+            }
+        }
+        catch
+        {
+            Write-Error "An error occurred while adding the $($role) role to $($this.Name)."
+            throw
+        }
+    }
+
+    <#
+    .SYNOPSIS
+    Removes a role from the server and cleans up related properties.
+
+    .DESCRIPTION
+    The RemoveRole method removes a specified role from the server. It also checks and removes
+    the properties associated with that role if they are not shared with other roles on the server.
+
+    .PARAMETER role
+    The role to be removed from the server. Must be a valid value from RelativityServerRole enum.
+
+    .EXAMPLE
+    $server.RemoveRole([RelativityServerRole]::Agent)
+    Removes the Agent role from the server.
+
+    .NOTES
+    #>
+    [void] RemoveRole([RelativityServerRole] $role)
+    {
+        try
+        {
+            if ($this.Role.Contains($role))
+            {
+                Write-Verbose "Removing $($role) role from $($this.Name)."
+                $this.Role.Remove($role)
+                foreach ($Property in [RelativityServer]::RoleResponseFileProperties[$role])
+                {
+                    $AllOtherRoles = $this.Role -ne $role
+                    $IsShared = $AllOtherRoles | ForEach-Object {
+                        [RelativityServer]::RoleResponseFileProperties[$_].Contains($Property)
+                    } | Where-Object { $_ }
+                    if (-not $IsShared)
+                    {
+                        Write-Verbose "Removing $($Property) Property from $($this.Name)."
+                        $this.ResponseFileProperties.Remove($Property)
+                        Write-Verbose "Removed $($Property) Property from $($this.Name)."
+                    }
+                }
+                Write-Verbose "Removed $($role) role from $($this.Name)."
+            }
+        }
+        catch
+        {
+            Write-Error "An error occurred while removing the $($role) role from $($this.Name)."
+            throw
+        }
+    }
+
+    <#
+    .SYNOPSIS
+    Sets a specific property for the server's configuration.
+
+    .DESCRIPTION
+    The SetProperty method assigns a value to a specific configuration property of the server.
+    It validates if the property is relevant to the server's assigned roles before setting it.
+
+    .PARAMETER property
+    The name of the property to set.
+
+    .PARAMETER value
+    The value to assign to the property.
+
+    .EXAMPLE
+    $server.SetProperty("InstallDir", "C:\InstallPath")
+    Sets the InstallDir property to the specified path.
+
+    .NOTES
+    #>
+    [void] SetProperty([String] $property, [String] $value)
+    {
+        try
+        {
+            if ($this.ResponseFileProperties.ContainsKey($property))
+            {
+                Write-Verbose "Setting $($property) Property to $($value) for $($this.Name)."
+                $this.ResponseFileProperties[$property] = $value
+                Write-Verbose "Set $($property) Property to $($value) for $($this.Name)."
+            }
+            else
+            {
+                Write-Error "$($property) Property is not valid for the roles assigned to $($this.Name)."
+            }
+        }
+        catch
+        {
+            Write-Error "An error occurred while setting the $($property) Property to $($value) for $($this.Name)."
+            throw
+        }
+    }
+
+    <#
+    .SYNOPSIS
+    Retrieves the configuration properties for the server.
+
+    .DESCRIPTION
+    The GetResponseFileProperties method compiles and returns a list of the server's configuration
+    properties formatted as key-value pairs. This is useful for generating configuration files or reports.
+
+    .EXAMPLE
+    $server.GetResponseFileProperties()
+    Returns an array of the server's properties in key=value format.
+
+    .RETURN
+    An array of strings, each representing a configuration property in key=value format.
+
+    .NOTES
+    #>
+    [String[]] GetResponseFileProperties()
+    {
+        try
+        {
+            $Properties = @()
+            foreach ($Key in $this.ResponseFileProperties.Keys)
+            {
+                $Value = $this.ResponseFileProperties[$Key]
+                $FormattedKey = $Key.ToUpper()
+                $Property = "$($FormattedKey)=$($Value)"
+                $Properties += $Property
+            }
+
+            return $Properties
+        }
+        catch
+        {
+            Write-Error "An error occurred while retrieving response file properties for $($this.Name)."
+            throw
+        }
+    }
+
+    <#
+    .SYNOPSIS
+    Tests network connectivity to the server.
+
+    .DESCRIPTION
+    The TestConnection method checks if the server is reachable over the network using a ping test.
+    It sets the IsOnline property based on the outcome of the test.
+
+    .EXAMPLE
+    $server.TestConnection()
+    Performs a connectivity test and updates the IsOnline property accordingly.
+
+    .NOTES
+    #>
+    [void] TestConnection()
+    {
+        Write-Verbose "Testing network connectivity to $($this.Name)."
+        if (Test-Connection -ComputerName $this.Name -Count 1 -Quiet)
+        {
+            $this.IsOnline = $true
+        }
+        else
+        {
+            Write-Warning "Network connectivity test for $($this.Name) was unsuccessful. Flagging server as offline."
+            $this.IsOnline = $false
+        }
+        Write-Verbose "Tested network connectivity to $($this.Name)."
+    }
+}
+
 class RelativityInstance
 {
     [ValidateNotNullOrEmpty()]
@@ -18,16 +393,18 @@ class RelativityInstance
     [ValidateNotNull()]
     [PSCredential] $ServiceAccountRelativityCredential
 
-    RelativityInstance([String] $name)
+    RelativityInstance([String] $name, [String] $friendlyName = $null)
     {
         $this.Name = $name
-        $this.FriendlyName = $name
-    }
 
-    RelativityInstance([String] $name, [String] $friendlyName)
-    {
-        $this.Name = $name
-        $this.FriendlyName = $friendlyName
+        if ($null -eq $friendlyName)
+        {
+            $this.FriendlyName = $name
+        }
+        else
+        {
+            $this.FriendlyName = $friendlyName
+        }
     }
 
     [void] AddServer([RelativityServer] $server)
@@ -308,57 +685,5 @@ class RelativityInstance
         $existingServer.NISTPackagePath = if ([String]::IsNullOrEmpty($existingServer.NISTPackagePath)) { $newServer.NISTPackagePath } else { $existingServer.NISTPackagePath }
 
         $this.Servers += $existingServer
-    }
-}
-
-class RelativityServer
-{
-    [ValidateNotNullOrEmpty()]
-    [String] $Name
-    [ValidateNotNull()]
-    [String[]] $Role
-    [String] $ServerFQDN
-    [String] $InstallDirectory
-    [String] $SecretStoreInstallDirectory
-    [String] $QueueManagerInstallDirectory
-    [String] $WorkerInstallDirectory
-    [String] $DefaultFileRepository
-    [String] $EDDSFileShare
-    [String] $CacheLocation
-    [String] $DtSearchIndexPath
-    [String] $DataFilesNetworkPath
-    [String] $SqlInstance
-    [Int32] $SqlPort
-    [Boolean] $UseWinAuth
-    [String] $SqlBackupDirectory
-    [String] $SqlLogDirectory
-    [String] $SqlDataDirectory
-    [String] $SqlFulltextDirectory
-    [String] $ServiceNamespace
-    [Boolean] $RabbitMQTLSEnabled
-    [Boolean] $EnableWinAuth
-    [String] $WorkerNetworkPath
-    [String] $IdentityServerUrl
-    [String] $NISTPackagePath
-
-    RelativityServer([String] $name)
-    {
-        $this.Name = $name
-        $this.Role = @()
-        $this.ServerFQDN = "$($name).$((Get-WmiObject -Namespace root\cimv2 -Class Win32_ComputerSystem).Domain)"
-        $this.InstallDirectory = "C:\Program Files\kCura Corporation\Relativity\"
-        $this.SecretStoreInstallDirectory = "C:\Program Files\Relativity Secret Store\"
-        $this.QueueManagerInstallDirectory = "C:\Program Files\kCura Corporation\Invariant\QueueManager\"
-        $this.WorkerInstallDirectory = "C:\Program Files\kCura Corporation\Invariant\Worker\"
-    }
-
-    [void] AddRole([String] $role)
-    {
-        if (-not ($role -in @("SecretStore", "PrimarySql", "DistributedSql", "ServiceBus", "Web", "Agent", "WorkerManager", "Worker")))
-        {
-            throw "$($role) is not an expected server role!"
-        }
-
-        $this.Role += $role
     }
 }
--- Public/New-RelativityServer.ps1
+++ Public/New-RelativityServer.ps1
@@ -1,3 +1,27 @@
+<#
+.SYNOPSIS
+  Creates a new instance of a RelativityServer.
+
+.DESCRIPTION
+  The New-RelativityServer function initializes a new instance of the RelativityServer class.
+  It takes a server name as input and returns an object representing a server within the Relativity environment.
+
+.PARAMETER Name
+  The name of the server to be created. This should be a valid network name.
+
+.EXAMPLE
+  New-RelativityServer -Name "Server01"
+  Creates a new RelativityServer instance named "Server01".
+
+.NOTES
+
+.INPUTS
+  None
+
+.OUTPUTS
+  RelativityServer
+  Returns an instance of the RelativityServer class.
+#>
 function New-RelativityServer
 {
     [CmdletBinding()]
@@ -10,7 +34,7 @@ function New-RelativityServer
 
     Begin
     {
-        Write-Verbose "Starting New-RelativityServer"
+        Write-Verbose "Starting New-RelativityServer."
     }
     Process
     {
@@ -22,12 +46,12 @@ function New-RelativityServer
         }
         catch
         {
-            Write-Error "An error occurred when creating a new Relativity server: $($_.Exception.Message)"
+            Write-Error "An error occurred when creating a new Relativity server: $($_.Exception.Message)."
             throw
         }
     }
     End
     {
-        Write-Verbose "Completed New-RelativityServer"
+        Write-Verbose "Completed New-RelativityServer."
     }
 }
